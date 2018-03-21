128c32607e30dad710a284024a05f654b50f4a65
suzto <36914663+suzto@users.noreply.github.com>
2018-03-22 02:06:00 +0800

    Update album_info.py

--- album_info.py
+++ album_info.py
@@ -5,8 +5,6 @@ import time
 import types
 import requests
 
-id_artist = '22033'
-
 
 # 获取网页信息
 def get_html(url):
@@ -19,59 +17,70 @@ def get_html(url):
     return html_data
 
 
+# 根据歌手id获取专辑列表
 def get_album(id_artist):
     url_artist = 'http://music.163.com/api/artist/albums/' + str(id_artist) + '?id='\
                  + str(id_artist) + '&offset=0&total=true&limit=1000'
     data_artist = get_html(url_artist)
+    id_list_album = []
     for dic_album in data_artist['hotAlbums']:
         name_group = dic_album['artist']['name']
         id_group = dic_album['artist']['id']
         name_album = dic_album['name']
         id_album = dic_album['id']
-        comment_url = dic_album['commentThreadId']
-        print(name_album)
-get_album(id_artist)
-
-def getAlbumList(id_Group):
-    url_Group = 'http://music.163.com/api/artist/albums/' + str(id_Group) + '?id=' + str(
-        id_Group) + '&offset=0&total=true&limit=1000'
-    page_Group = get_html(url_Group)
-    # 反向寻找专辑id
-    reg0 = r'"buSsi"(.+?)"eman"'
-    abmdata = getRegex(reg0, page_Group.decode()[::-1])
-    abmlist = []
-    for abms in abmdata:
-        abms = abms[::-1]
-        abmnm = getRegex('"(.+?)","id', abms)[0]
-        abmid = getRegex('"id":(\d+),', abms)[0]
-        abmlist.append([abmnm, abmid])
-    return abmlist
+        comment_url_album = dic_album['commentThreadId']
+        # 将以上信息存入数据库album
+        id_list_album.append(id_album)
+    return id_list_album
 
 
-# 根据专辑id获取歌词页面
-def getTrackList(id_Album, savetxt):
-    url_Album = 'http://music.163.com/api/album/' + str(id_Album) + '?ext=true&id=' + str(
-        id_Album) + '&offset=0&total=true&limit=1000'
-    page_album = getHtml(url_Album)
-    page_rev = page_album.decode()[::-1]
-    reg1 = r'"R_SO_4_(\d+)"'
-    songid_total = getRegex(reg1, page_album.decode())
-    # writelinez = []
-    for songid in songid_total:
-        song_name = getRegex(songid[::-1] + r':"di","(.+?)"', page_rev)[0][::-1]
+# 根据专辑id获取歌曲页面
+def get_track(id_album):
+    url_album = 'http://music.163.com/api/album/' + str(id_album) + '?ext=true&id='\
+                + str(id_album) + '&offset=0&total=true&limit=1000'
+    data_album = get_html(url_album)
+    comment_count_album = data_album['album']['info']['commentCount']
+    share_count_album = data_album['album']['info']['shareCount']
+    size_album = data_album['album']['size']
+    # 将以上信息存入数据库album
+    id_list_track = []
+    for dic_track in data_album['album']['songs']:
+        popularity_track = dic_track['popularity']
+        comment_count_track = dic_track['commentThreadId']
+        duration_track = dic_track['duration']
+        copyright_track = dic_track['copyrightId']
+        artists_track = []
+        for dic_artist in dic_track['artists']:
+            artists_track.append(dic_artist['name'])
+        name_track = dic_track['name']
+        id_track = dic_track['id']
+        position_track = dic_track['position']
+        # 将以上信息存入数据库track(加上歌曲在专辑的次序数）
+        id_list_track.append(id_track)
+    return id_list_track
 
 
-def songpage(sid, sname, savetxt):
-    writelinez = ['\n\n################################\n\n', sname,
-                  '\n\n#####################################\n\n']
-    url_Track = 'http://music.163.com/api/song/lyric?os=pc&id=' + str(sid) + '&lv=-1&kv=-1&tv=-1'
-    page_song = getHtml(url_Track)
-    if len(page_song) > 150:
-        try:
-            print(sname)
-            writelinez.extend(lrctotxt(page_song.decode()))
-            with open(savetxt, 'a', encoding='utf-8') as file:
-                for writex in writelinez:
-                    file.write('%s' % (writex))
-        except (KeyError, IndexError):
-            appenddoc('rubbish', str(sid) + '\n')
+# 根据歌曲id获取评论
+def get_comment(id_track):
+    pages = 0
+    url_comment = 'http://music.163.com/api/v1/resource/comments/R_SO_4_' \
+                  + str(id_track) + '?limit=20&offset=' + str(pages)
+    # url_lyric = 'http://music.163.com/api/song/lyric?os=pc&id=' + str(sid) + '&lv=-1&kv=-1&tv=-1'
+    page_track = get_html(url_comment)
+    total_track_comment = page_track['total']
+    return total_track_comment
+
+
+if __name__ == '__main__':
+    id_test = '12451165'
+    # get_track(id_test)
+    # get_album(id_test)
+    # get_comment(id_test)
+    total_num_comment = 0
+    id_albums = get_album(id_test)
+    for album in id_albums:
+        id_tracks = get_track(album)
+        for track in id_tracks:
+            total_num_comment += get_comment(track)
+        print(total_num_comment)
+    print(total_num_comment)
